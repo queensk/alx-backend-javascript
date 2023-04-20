5f104053c53213e0bc80075de7c22f82d0b51c25
queensk <queenskisivuli@gmail.com>
2023-04-20 04:37:29 +0300

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+// get full promise
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: "Success" });
+    } else {
+      reject(new Error("The fake API is not working currently"));
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
