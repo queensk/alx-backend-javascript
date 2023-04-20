d33dc7bebf79e2c7c2dc2d6c604c4c5397bb5fe0
queensk <queenskisivuli@gmail.com>
2023-04-20 20:12:12 +0300

    fix lint

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,26 +1,29 @@
 // price
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
-export default class Pricing{
-    constructor(amount, currency){
-        this._amount = typeof amount === "number"? amount : (()=>{throw new Error("Amount must be a number")})();
-        this._currency = currency instanceof Currency? currency: (()=>{ throw new Error("Currency must be a Currency")})();
-    }
-    get amount(){
-        return this._amount
-    }
-    set amount(amount){
-        this._amount = typeof amount === "number"? amount : (()=>{throw new Error("Amount must be a number")})();
-    }
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = typeof amount === 'number' ? amount : (() => { throw new Error('Amount must be a number'); })();
+    this._currency = currency instanceof Currency ? currency : (() => { throw new Error('Currency must be a Currency'); })();
+  }
 
-    get currency(){
-        return this._currency
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    set currency(currency){
-        this._currency = currency instanceof Currency? currency: (()=>{throw new Error("Currency must be a Currency")})();
-    }
-    displayFullPrice(){
-        return `${this._amount} ${this.currency.name} (${this.currency.code})`
-    }
+  set amount(amount) {
+    this._amount = typeof amount === 'number' ? amount : (() => { throw new Error('Amount must be a number'); })();
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency instanceof Currency ? currency : (() => { throw new Error('Currency must be a Currency'); })();
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+  }
 }
