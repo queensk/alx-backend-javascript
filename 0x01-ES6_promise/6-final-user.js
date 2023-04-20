5ff01332f152350086f38d1cbd8fa40793207d8c
queensk <queenskisivuli@gmail.com>
2023-04-20 10:35:51 +0300

    update the handle profile signup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => response.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: 'fulfilled', value: result.value };
-      }
-      return { status: 'rejected', value: result.reason };
-    }));
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
+
+export default handleProfileSignup;
--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,3 +1,3 @@
 import handleProfileSignup from './6-final-user';
 
-console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg").then((response)=> console.log(response)));
