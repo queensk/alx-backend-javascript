cde3abaecbd1a26a596ae14fbeb86cc83eac7855
queensk <queenskisivuli@gmail.com>
2023-04-20 11:19:19 +0300

    try divding by 0

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+// Throw error / try catch
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
