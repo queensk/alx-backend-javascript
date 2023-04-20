0690221e16b1f0fd51e7584b60f29e93b66a89c4
queensk <queenskisivuli@gmail.com>
2023-04-20 08:50:15 +0300

    change default export

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,6 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
-
-export default handleProfileSignup;
