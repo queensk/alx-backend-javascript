2ea92c211898346558ac63eb4580206c925d4b8b
queensk <queenskisivuli@gmail.com>
2023-04-20 05:50:20 +0300

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+// Handle multiple promises
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+}
