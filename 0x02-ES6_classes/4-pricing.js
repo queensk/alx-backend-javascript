02400e3afe1f3730d9edaeb73e0133b9d9b20942
queensk <queenskisivuli@gmail.com>
2023-04-20 20:10:15 +0300

    currency Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+// price
+import Currency from "./3-currency";
+
+export default class Pricing{
+    constructor(amount, currency){
+        this._amount = typeof amount === "number"? amount : (()=>{throw new Error("Amount must be a number")})();
+        this._currency = currency instanceof Currency? currency: (()=>{ throw new Error("Currency must be a Currency")})();
+    }
+    get amount(){
+        return this._amount
+    }
+    set amount(amount){
+        this._amount = typeof amount === "number"? amount : (()=>{throw new Error("Amount must be a number")})();
+    }
+
+    get currency(){
+        return this._currency
+    }
+
+    set currency(currency){
+        this._currency = currency instanceof Currency? currency: (()=>{throw new Error("Currency must be a Currency")})();
+    }
+    displayFullPrice(){
+        return `${this._amount} ${this.currency.name} (${this.currency.code})`
+    }
+}
