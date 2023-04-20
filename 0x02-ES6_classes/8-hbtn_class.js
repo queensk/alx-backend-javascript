8a5791f9b40e71faae388203c317556de1a94239
queensk <queenskisivuli@gmail.com>
2023-04-20 22:54:43 +0300

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,12 @@
+// HolbertonClass
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'string') return this._location;
+    return this._size;
+  }
+}
