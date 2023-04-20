97c07e9e73d85777be5d5cd818829daf4e3e52a2
queensk <queenskisivuli@gmail.com>
2023-04-20 04:45:51 +0300

    fix eslint format

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,15 @@
 // get full promise
 function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: "Success" });
-    } else {
-      reject(new Error("The fake API is not working currently"));
-    }
-  });
-}
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }  
 
 export default getFullResponseFromAPI;
