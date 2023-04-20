a9e6f0551d12af513f123e55f653560e2722ac04
queensk <queenskisivuli@gmail.com>
2023-04-20 20:54:55 +0300

    fix lint

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,26 +1,30 @@
 // Inheritance
-import Building from "./5-building";
- 
-export default class SkyHighBuilding extends Building{
-    constructor(sqft, floors){
-        super(sqft)
+import Building from './5-building';
 
-        this._floors = floors
-    }
-        
-    get sqft(){
-        return this._sqft
-    }
-    set sqft(area){
-        this._sqft = area
-    }
-    get floors (){
-        return this._floors
-    }
-    set floors(floors){
-        this._floors = floors
-    }
-    evacuationWarningMessage(){
-       return `Evacuate slowly the ${this.floors} floors`
-    }
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(area) {
+    this._sqft = area;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
