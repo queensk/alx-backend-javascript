59749b26706944b7597d6dad64d49a84607e0a9e
queensk <queenskisivuli@gmail.com>
2023-04-20 08:53:28 +0300

    update return indent

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
