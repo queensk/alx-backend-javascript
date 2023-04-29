91eb8eb1b9cc6d97674b6efb25368bb48250c768
queensk <queenskisivuli@gmail.com>
2023-04-29 05:33:57 +0300

    type script

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    [propName: string]: any,
+    
+}
