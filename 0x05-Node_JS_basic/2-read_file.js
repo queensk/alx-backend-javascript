86edf42fddf5c7e3540620921a56e52b77027d7b
queensk <queenskisivuli@gmail.com>
2023-06-17 09:19:37 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,39 +1,21 @@
-// 2-read_file.js
 const fs = require('fs');
 
-function countStudents (path) {
+const countStudents = (aPath) => {
   try {
-    // read the file synchronously
-    const data = fs.readFileSync(path, 'utf8');
-    // split the data by line
-    const lines = data.split('\n');
-    // remove the header and any empty lines
-    lines.shift();
-    lines.filter((line) => line !== '');
-    // initialize an object to store the number and list of students by field
-    const fields = {};
-    // loop through each line
-    for (const line of lines) {
-      // split the line by comma
-      const [firstName, , , field] = line.split(',');
-      // if the field is not in the object, create a new entry with an empty array
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      // push the first name to the array
-      fields[field].push(firstName);
+    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
     }
-    // log the total number of students
-    console.log(`Number of students: ${lines.length}`);
-    // loop through each field in the object
-    for (const [field, students] of Object.entries(fields)) {
-      // log the number and list of students in each field
-      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    for (const subject in subjects) {
+      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
     }
   } catch (error) {
-    // if the file is not available, throw an error
     throw new Error('Cannot load the database');
   }
-}
-
+};
 module.exports = countStudents;
