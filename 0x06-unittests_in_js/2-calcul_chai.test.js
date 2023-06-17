8482d9a390f6f070118c1d0de607bc345762e8b8
queensk <queenskisivuli@gmail.com>
2023-06-17 16:06:48 +0300

    fixed semai stundered

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,4 @@
-var chai = require('chai');
+const chai = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function () {
--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,6 +1,6 @@
 const utils = require('./utils');
 
-module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
+module.exports = function sendPaymentRequestToApi (totalAmount, totalShipping) {
   const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
   return total;
