d8be578ef5d1287ddb3890abd80d86dec3570ba4
queensk <queenskisivuli@gmail.com>
2023-06-17 10:09:54 +0300

    node server 2

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,11 @@
-// 4-http.js
 const http = require('http');
 
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-  res.statusCode(200);
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello Holberton School!');
-  res.end();
+  res.end('Hello Holberton School!');
 });
-
-app.listen(1245);
-
+app.listen(port);
 module.exports = app;
